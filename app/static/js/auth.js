11568eea33c928c26668396e405ccae4019879d0
zanovex <zanodysseys@gmail.com>
2026-02-25 21:47:39 +0300

    Update auth.js

--- app/static/js/auth.js
+++ app/static/js/auth.js
@@ -1,25 +1,19 @@
-async function login(event) {
-  event.preventDefault();
-
+async function login() {
   const username = document.getElementById("username").value;
   const password = document.getElementById("password").value;
 
-  const formData = new FormData();
-  formData.append("username", username);
-  formData.append("password", password);
-
-  const res = await fetch("/api/v1/auth/login", {
-    method: "POST",
-    body: formData
-  });
+  const res = await fetch(
+    `/api/v1/auth/login?username=${username}&password=${password}`,
+    { method: "POST" }
+  );
 
   const data = await res.json();
 
   if (!res.ok) {
-    document.getElementById("error").innerText =
-      data.detail || "Invalid username or password";
-    return;
-  }
+  document.getElementById("error").innerText =
+    data.detail || "Invalid username or password";
+  return;
+}
 
   localStorage.setItem("token", data.access_token);
   window.location.href = "/dashboard";
